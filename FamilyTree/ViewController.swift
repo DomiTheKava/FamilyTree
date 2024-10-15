63890ff397d0df9c93b487222089277ecf8afe7e
DomiTheKava <Dominikpenkava@yahoo.com>
2024-10-15 17:16:41 -0500

    Initial Commit

---
+++ FamilyTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilyTree
+//
+//  Created by Dominik Penkava on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
